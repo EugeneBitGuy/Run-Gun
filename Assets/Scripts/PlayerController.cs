629b7b16e6103f5a13e6cd2698f34169f737d4b7
Евгений Постоенко <postoinko@gmail.com>
2021-11-03 00:02:42 +0200

    Added Player-Capsule, Player-Movement scripts, swipe controller scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    [SerializeField] private int speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (SwipeController.swipeUp && controller.isGrounded)
+            Jump();
+        
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        transform.position = targetPosition;
+    }
+
+    void FixedUpdate()
+    {
+        direction.z = speed;
+        direction.y += gravity * Time.fixedDeltaTime;
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+}
