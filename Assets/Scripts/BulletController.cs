0d6edb46bdade93d83efeaaa179f47517d071161
Евгений Постоенко <postoinko@gmail.com>
2021-11-03 16:35:00 +0200

    Added shooting scripts for player

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float lifetime;
+    [SerializeField] private float distance;
+    [SerializeField] private int damage;
+
+    public LayerMask whatIsSolid;
+
+    private void Update()
+    {
+        RaycastHit hitInfo;
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, distance, whatIsSolid))
+        {
+            if (hitInfo.collider.tag == "Obstacle")
+            {
+                hitInfo.collider.GetComponent <ObstacleController> ().TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+        
+
+        lifetime -= Time.deltaTime;
+
+        if(lifetime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShoterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoterController : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform shotPoint;
+
+    private float timeBtwShots;
+    [SerializeField] private float startTimeBtwShots;
+    void Update()
+    {
+        if(timeBtwShots <= 0) { 
+            if (Input.GetMouseButton(1))
+            {
+                Instantiate(bullet, shotPoint.position, transform.rotation);
+            }
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
