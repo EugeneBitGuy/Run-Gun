4d8acb919b4358a26875d6b1c5dd0eabd3b7b4f2
Евгений Постоенко <postoinko@gmail.com>
2021-11-03 20:20:23 +0200

    added level generator
    -addition:
    fixed camera(added following script)

---
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 offset;
+    private float startYPosition;
+    private void Awake()
+    {
+        offset = transform.position - target.position;
+        startYPosition = transform.position.y;
+    }
+    private void LateUpdate()
+    {
+        Vector3 positionToSet = target.position + offset;
+        positionToSet.y = startYPosition;
+        transform.position = positionToSet;
+
+    }
+}
---
+++ Assets/Scripts/LevelGeneratorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneratorController : MonoBehaviour
+{
+    public GameObject[] roads;
+    private List<GameObject> usedRoads = new List<GameObject>();
+    private float spawnPos = 0;
+    private float roadLength = 100;
+
+    [SerializeField] private Transform player;
+    private int startRoads = 5;
+
+    private void Start()
+    {
+        SpawnRoad(0);
+
+        for (int i = 0; i < startRoads; i++)
+            SpawnRoad(Random.Range(1, roads.Length));
+    }
+
+    private void Update()
+    {
+        if (player.position.z - 60 > spawnPos - (startRoads * roadLength)){
+            SpawnRoad(Random.Range(1, roads.Length));
+            DeleteRoad();
+        }
+    }
+
+    private void SpawnRoad(int index)
+    {
+        GameObject nextRoad = Instantiate(roads[index], transform.forward * spawnPos, transform.rotation);
+        usedRoads.Add(nextRoad);
+        spawnPos += roadLength;
+    }
+
+    private void DeleteRoad()
+    {
+        Destroy(usedRoads[0]);
+        usedRoads.RemoveAt(0);
+    }
+}
