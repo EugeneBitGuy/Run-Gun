80f5f3966c275b35050fccdeac6b07030ad854d9
Евгений Постоенко <postoinko@gmail.com>
2021-11-04 20:14:31 +0200

    Added Crouch feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,26 @@ public class PlayerController : MonoBehaviour
     private Vector3 direction;
     [SerializeField] private int speed;
     [SerializeField] private float jumpForce;
-    [SerializeField] private float gravity;
+    [SerializeField] private float commonGravity;
+    private Vector3 defaultScale;
+    [SerializeField] private float crouchTime;
+    [SerializeField] private float crouchGravity;
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        defaultScale = transform.localScale;
     }
 
     void Update()
     {
         if (SwipeController.swipeUp && controller.isGrounded)
             Jump();
-        
 
+        if (SwipeController.swipeDown)
+        {
+            StartCoroutine(Crouch());
+        }
+        
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
 
         transform.position = targetPosition;
@@ -28,12 +36,22 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         direction.z = speed;
-        direction.y += gravity * Time.fixedDeltaTime;
+        direction.y += (-commonGravity) * Time.fixedDeltaTime;
         controller.Move(direction * Time.fixedDeltaTime);
     }
 
     private void Jump()
     {
         direction.y = jumpForce;
+    }
+    private IEnumerator Crouch()
+    {
+        commonGravity += crouchGravity;
+        transform.localScale = new Vector3(defaultScale.x, defaultScale.y * 0.5f, defaultScale.z);
+
+        yield return new WaitForSeconds(0.5f);
+
+        transform.localScale = defaultScale;
+        commonGravity -= crouchGravity;
     }
 }
