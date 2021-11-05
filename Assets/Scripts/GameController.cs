81e38a4a5fb6d9084b83673f8dfd0813232b5f35
Евгений Постоенко <postoinko@gmail.com>
2021-11-05 18:46:21 +0200

    added game over feature
    hp for player;
    hp descrease if entered obstacle
    game over if hp is less than zero

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private static bool isGameOver;
+    void Start()
+    {
+        isGameOver = false;
+    }
+    void Update()
+    {
+        if (isGameOver)
+        {
+            Time.timeScale = 0;
+            Debug.Log("GameOver");
+        }
+    }
+
+    public static void GameOver()
+    {
+        isGameOver = true;
+    }
+
+}
